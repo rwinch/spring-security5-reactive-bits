6661631cc58d7627cbab02eef64b79b5a3459ef5
Rob Winch <rwinch@users.noreply.github.com>
2017-12-04 15:52:14 -0600

    Custom Log In Page

--- src/main/java/sample/security/SecurityConfig.java
+++ src/main/java/sample/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package sample.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * @author Rob Winch
@@ -14,6 +16,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class SecurityConfig {
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http
+			.authorizeExchange()
+				.pathMatchers("/login", "/signup", "/webjars/**").permitAll()
+				.anyExchange().authenticated()
+				.and()
+			.httpBasic().and()
+			.formLogin()
+				.loginPage("/login");
+		return http.build();
+	}
+
 	// @formatter:off
 	@Bean
 	MapReactiveUserDetailsService userDetailsService() {
