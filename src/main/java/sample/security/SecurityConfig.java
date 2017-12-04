af79966a0f04b2296d7b6f75734551a2485ec99b
Rob Winch <rwinch@users.noreply.github.com>
2017-12-04 15:48:05 -0600

    MapReactiveUserDetailsService withUsername

---
+++ src/main/java/sample/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package sample.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @author Rob Winch
+ * @since 5.0
+ */
+@Configuration
+public class SecurityConfig {
+	// @formatter:off
+	@Bean
+	MapReactiveUserDetailsService userDetailsService() {
+		UserDetails rob = User.withUsername("rob@example.com")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new MapReactiveUserDetailsService(rob);
+	}
+	// @formatter:on
+}
