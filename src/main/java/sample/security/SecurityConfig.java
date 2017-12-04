2f456b421c697346f17848df48bf57cb1b6c2aae
Rob Winch <rwinch@users.noreply.github.com>
2017-12-04 16:28:10 -0600

    Authorization SererHttpSecurity isRob

--- src/main/java/sample/security/SecurityConfig.java
+++ src/main/java/sample/security/SecurityConfig.java
@@ -2,10 +2,14 @@ package sample.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import reactor.core.publisher.Mono;
 
 /**
  * @author Rob Winch
@@ -17,6 +21,7 @@ public class SecurityConfig {
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http
 			.authorizeExchange()
+				.pathMatchers("/users").access(this::isRob)
 				.pathMatchers("/login", "/signup", "/webjars/**").permitAll()
 				.anyExchange().authenticated()
 				.and()
@@ -24,6 +29,14 @@ public class SecurityConfig {
 			.formLogin()
 				.loginPage("/login");
 		return http.build();
+	}
+
+	private Mono<AuthorizationDecision> isRob(Mono<Authentication> authentication,
+			AuthorizationContext authorizationContext) {
+		return authentication
+				.map(Authentication::getName)
+				.map(username -> username.startsWith("rob@"))
+				.map(AuthorizationDecision::new);
 	}
 
 	@Bean
